36f4e0b03adc9366632d75b98e65ee2ae5e4fac9
Avni C <avni27reddyc@gmail.com>
2023-06-23 13:54:16 -0400

    Initial Commit

---
+++ buttons/buttonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonsApp.swift
+//  buttons
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct buttonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
